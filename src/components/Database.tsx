b5e6a6b3d191b28b3dd846cbf8f4f930a347c5c4
salmanahmed221 <salmanahmed121221@gmail.com>
2024-03-16 15:51:19 +0500

    add new project in portfolio

--- src/components/Database.tsx
+++ src/components/Database.tsx
@@ -1,50 +1,56 @@
 export const ProjectsData = [
-    {
-        id: 1,
-        link: "https://next-js-and-tailwind-css-template.vercel.app/",
-        image: "/Bussiness.png",
-        title: "Bussiness Template"
-    },
-    {
-        id: 2,
-        link: "https://chakra-landing-page-with-pages-directory.vercel.app/",
-        image: "/ChakraLanding.png",
-        title: "Landing Page"
-    },
-    {
-        id: 3,
-        link: "https://tailwind-catering-template.vercel.app/",
-        image: "/Cathering.png",
-        title: "Cathering Template"
-    },
-    {
-        id: 4,
-        link: "https://chakra-ui-pricing-project.vercel.app/",
-        image: "/pricing.png",
-        title: "Pricing Ui"
-    },
-    {
-        id: 5,
-        link: "https://panaverse-practice-single-page.vercel.app/",
-        image: "/Panaverse.png",
-        title: "Panaverse Website Design"
-    },
-    {
-        id: 6,
-        link: "https://tailwind-css-panaverse-dao-project.vercel.app/",
-        image: "/PanaverseSite.png",
-        title: "Panaverse Dao Project"
-    },
-    {
-        id: 7,
-        link: "https://hackaton-one-eight.vercel.app/",
-        image: "/dinemarket.png",
-        title: "Dine Market Store"
-    },
-    {
-        id: 8,
-        link: "https://permabull-indol.vercel.app/",
-        image: "/permabull.png",
-        title: "Permabull"
-    },
-]
+  {
+    id: 1,
+    link: "https://next-js-and-tailwind-css-template.vercel.app/",
+    image: "/Bussiness.png",
+    title: "Bussiness Template",
+  },
+  {
+    id: 2,
+    link: "https://chakra-landing-page-with-pages-directory.vercel.app/",
+    image: "/ChakraLanding.png",
+    title: "Landing Page",
+  },
+  {
+    id: 3,
+    link: "https://tailwind-catering-template.vercel.app/",
+    image: "/Cathering.png",
+    title: "Cathering Template",
+  },
+  {
+    id: 4,
+    link: "https://chakra-ui-pricing-project.vercel.app/",
+    image: "/pricing.png",
+    title: "Pricing Ui",
+  },
+  {
+    id: 5,
+    link: "https://panaverse-practice-single-page.vercel.app/",
+    image: "/Panaverse.png",
+    title: "Panaverse Website Design",
+  },
+  {
+    id: 6,
+    link: "https://tailwind-css-panaverse-dao-project.vercel.app/",
+    image: "/PanaverseSite.png",
+    title: "Panaverse Dao Project",
+  },
+  {
+    id: 7,
+    link: "https://hackaton-one-eight.vercel.app/",
+    image: "/dinemarket.png",
+    title: "Dine Market Store",
+  },
+  {
+    id: 8,
+    link: "https://permabull-indol.vercel.app/",
+    image: "/permabull.png",
+    title: "Permabull",
+  },
+  {
+    id: 9,
+    link: "https://destra-three.vercel.app/",
+    image: "/destra.png",
+    title: "Destra Network",
+  },
+];
